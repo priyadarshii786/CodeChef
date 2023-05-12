7f1e8c0e53827028a9b85498573d8956ab941f7d
priyadarshii786 <tathagatp918@gmail.com>
2023-05-13 04:35:38 +0530

    this is the first commit bro!!

---
+++ START 75D/TAXSAVING.cpp
@@ -0,0 +1,23 @@
+/*------26/01/2023------*/
+#include <bits/stdc++.h> //jai ganesh
+using namespace std;
+
+void solution()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << (x - y) << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t > 0)
+    {
+        solution();
+        t--;
+    }
+
+    return 0;
+}
