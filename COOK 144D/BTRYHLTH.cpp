e1cbe13ffb78956ec46f94a77bce126cd15c5f30
priyadarshii786 <tathagatp918@gmail.com>
2023-05-21 23:21:15 +0530

    11.21pm

---
+++ COOK 144D/BTRYHLTH.cpp
@@ -0,0 +1,29 @@
+/*------07/02/2023------*/
+#include <bits/stdc++.h> //jai ganesh
+using namespace std;
+
+void solution()
+{
+    int percent;
+    cin >> percent;
+
+    if (percent >= 80)
+        cout << "YES" << endl;
+
+    else
+        cout << "NO" << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t > 0)
+    {
+        solution();
+        t--;
+    }
+    // your code goes here
+
+    return 0;
+}
